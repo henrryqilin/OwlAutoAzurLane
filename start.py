15afd934ccf77348c3fc4464b8b4dad2616b56cb
henrryqilin <115403516+henrryqilin@users.noreply.github.com>
2022-12-21 15:47:25 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -1,4 +1,10 @@
-import Azur_Lane as oaa
+﻿import Azur_Lane as oaa
 #import develop as dev
 
-oaa.daily_challenge()
+"""
+oaa.find_and_start()    #刷关
+oaa.exercises()         #演习
+oaa.daily_challenge()   #每日挑战
+"""
+
+oaa.find_and_start()
